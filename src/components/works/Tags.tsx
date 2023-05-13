1115340c9e8007651401ca755d4b65222277a232
harutas <96802323+harutas@users.noreply.github.com>
2023-05-14 01:40:50 +0900

    fix: works tag wrap

--- src/components/works/Tags.tsx
+++ src/components/works/Tags.tsx
@@ -5,10 +5,13 @@ interface Props {
 }
 
 export const Tags = ({ tags }: Props) => {
+  // 表示するタグ数を制限
+  const limit = 4;
+  const filteredTags = tags?.slice(0, limit);
   return (
-    <div className="flex items-end justify-start">
-      {tags &&
-        tags.map((tag) => (
+    <div className="flex items-end justify-start flex-wrap">
+      {filteredTags &&
+        filteredTags.map((tag) => (
           <span
             key={tag.id}
             className="text-sm font-semibold inline-block py-1 px-2 rounded-lg text-sky-600 bg-sky-200 first:ml-0 last:mr-0  mr-1"
