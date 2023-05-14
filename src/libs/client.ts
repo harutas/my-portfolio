deb6c5fe236942e8f151c8b1366abd4d0b397a8e
harutas <96802323+harutas@users.noreply.github.com>
2023-05-15 00:50:07 +0900

    fix: cms custom fetch

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -33,23 +33,23 @@ if (!process.env.NEXT_PUBLIC_API_KEY) {
   throw new Error("NEXT_PUBLIC_API_KEY is required");
 }
 
-export const client = createClient({
-  serviceDomain: process.env.NEXT_PUBLIC_SERVICE_DOMAIN || "", // service-domain は XXXX.microcms.io の XXXX 部分
-  apiKey: process.env.NEXT_PUBLIC_API_KEY || "",
-});
 // export const client = createClient({
 //   serviceDomain: process.env.NEXT_PUBLIC_SERVICE_DOMAIN || "", // service-domain は XXXX.microcms.io の XXXX 部分
 //   apiKey: process.env.NEXT_PUBLIC_API_KEY || "",
-//   customFetch: (input, init) => {
-//     if (typeof input === "string") {
-//       const newInput = new URL(input);
-//       const time = new Date();
-//       newInput.searchParams.set("cacheclearparam", `${time.getMinutes()}`);
-//       return fetch(newInput.href, init);
-//     }
-//     return fetch(input, init);
-//   },
 // });
+export const client = createClient({
+  serviceDomain: process.env.NEXT_PUBLIC_SERVICE_DOMAIN || "", // service-domain は XXXX.microcms.io の XXXX 部分
+  apiKey: process.env.NEXT_PUBLIC_API_KEY || "",
+  customFetch: (input, init) => {
+    if (typeof input === "string") {
+      const newInput = new URL(input);
+      const time = new Date();
+      newInput.searchParams.set("cacheclearparam", `${time.getMinutes()}`);
+      return fetch(newInput.href, init);
+    }
+    return fetch(input, init);
+  },
+});
 
 export const getWorksList = async (queries?: MicroCMSQueries) => {
   const listData = await client.getList<Work>({
