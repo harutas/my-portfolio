ab68eb971e283329b9c67192a106bc44c137c85a
harutas <96802323+harutas@users.noreply.github.com>
2023-07-15 15:01:09 +0900

    fix: fontsize

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -6,23 +6,23 @@ const ProfilePage = () => {
   return (
     <div className='flex flex-col justify-center items-center h-screen-minus-header'>
       <h2 className='sm:text-6xl text-5xl my-8'>About Me</h2>
-      <div className='w-1/2'>
-        <p className='text-lg mb-5'>
-          コンピュータサイエンスが学べるプラットフォーム「Recursion」でコンピュータサイエンスを基礎から勉強しています。
+      <div className='sm:w-1/2 px-4'>
+        <p className='text-base mb-5'>
+          2021年10月からコンピュータサイエンスが学べるプラットフォーム「Recursion」でコンピュータサイエンスを基礎から勉強しています。
           <br />
           個人でもReactやVue.jsを使用したWebアプリの開発を行っています。
         </p>
       </div>
       <div className='max-w-md mx-auto p-4'>
-        <div className='flex space-x-4'>
+        <div className='flex space-x-16'>
           <a
             href='https://github.com/harutas/harutas'
             target='_blank'
             rel='noopener noreferrer'
             className='flex items-center text-blue-500 hover:underline'
           >
-            <Image src={githubIcon} className='rounded-full' width={40} alt='' />
-            <span className='text-xl ml-2'>GitHub</span>
+            <Image src={githubIcon} className='rounded-full' width={35} alt='' />
+            <span className='text-base ml-2'>GitHub</span>
           </a>
           <a
             href='https://recursionist.io/users/harutas'
@@ -31,7 +31,7 @@ const ProfilePage = () => {
             className='flex items-center text-blue-500 hover:underline'
           >
             <Image src={recursionIcon} className='rounded-full' width={40} alt='' />
-            <span className='text-xl ml-2'>Recursion</span>
+            <span className='text-base ml-2'>Recursion</span>
           </a>
         </div>
       </div>
